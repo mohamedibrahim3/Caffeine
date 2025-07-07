f2fd4292d51720cd941e315f0184e7850c87d52b
mada <mohamedibra3232@gmail.com>
2025-07-07 14:41:27 +0300

    feat: add font and colors design system

---
+++ app/src/main/java/com/example/caffeine/ui/theme/Font.kt
@@ -0,0 +1,17 @@
+package com.example.caffeine.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.caffeine.R
+
+val Sniglet = FontFamily(
+    Font(R.font.sniglet, FontWeight.Normal),
+    Font(R.font.sniglet_extrabold, FontWeight.ExtraBold)
+)
+
+val Urbanist = FontFamily(
+    Font(R.font.urbanist, FontWeight.Normal),
+    Font(R.font.urbanist_medium, FontWeight.Medium),
+    Font(R.font.urbanist_bold, FontWeight.Bold)
+)
